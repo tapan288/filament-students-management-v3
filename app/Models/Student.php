1fabbd93b3537a842950f80a4f9309f8d4e32d00
Tapan Sharma <tapansharma69@gmail.com>
2023-12-19 22:41:00 +0545

    define custom guard for student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Student extends Model
+class Student extends Authenticatable
 {
     use HasFactory;
+
+    protected $guard = 'student';
 
     protected $fillable = [
         'class_id',
