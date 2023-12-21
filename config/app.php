a3518c8ead14485b4f6c1d0e2804428f4d7a2f55
Tapan Sharma <tapansharma69@gmail.com>
2023-12-21 16:26:20 +0545

    define separate panel for student

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
+        App\Providers\Filament\StudentPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
